da2f0338d72db7a128548df459830b9bde4faa98
DESKTOP-VLB7TDB\Beka <beka.pukhashvili@gmail.com>
2023-05-15 23:10:01 +0400

    tasks created

--- Task16/Task16/Program.cs
+++ Task16/Task16/Program.cs
@@ -6,5 +6,36 @@
         {
 
         }
+
+        // Write method that accepts list of numbers and returns sum
+        public static int SumOfNumbersInList(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Write method that accepts list of strings and returns same list, but without duplicates
+        public static List<string> RemoveDuplicates(List<string> words)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Write method that accepts list of numbers and returns sum of minimum and maximum numbers in this lists
+        public static int SumOfMinAndMax(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Find the most frequent element in list and return it
+        public static int MostFrequendElement(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Bonus points
+        //write function that will accept 3D list and will return sum of all elements
+        public static int SumOfAllLists(List<List<List<int>>> list3D)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
