13a1ce5701d413814f7065a8e9f113a6da8bdfbe
Beka Pukhashvili <beka.pukhashvili@dataart.com>
2023-05-18 12:33:47 +0400

    added tasks

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Task
+﻿using System.Data;
+using System.Xml.Linq;
+using System;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+
+namespace Task
 {
     internal class Program
     {
@@ -6,5 +14,63 @@
         {
             Console.WriteLine("Hello, World!");
         }
+    }
+
+    //Task 1
+    //Create a simple Employee class. The class should have the following private properties:
+    //firstName,
+    //lastName,
+    //employeeId,
+    //salary,
+    //role.
+
+    //Implement a constructor that takes these values as parameters and initializes the properties.
+    //Implement public getters and setters for each of these properties.
+    //Make sure that the setter methods validate the inputs (for example, employeeId and salary cannot be negative, names should not be empty, etc.).
+    //Implement a method getFullName() that returns the full name of the employee.
+
+    class Employee
+    {
+
+    }
+
+    //Task 2
+    //Create a BankAccount class. The class should have the following private properties:
+    //accountNumber,
+    //ownerName,
+    //balance.
+
+    //Implement a constructor that takes these values as parameters and initializes the properties.
+    //The initial balance should be 0.
+
+    //Implement public getters and setters for accountNumber and ownerName, but only a getter for balance(the balance should not be directly settable).
+    //Implement two methods: deposit(amount) and withdraw(amount).
+    //The deposit(amount) method should add the amount to the balance.
+    //The withdraw(amount) method should subtract the amount from the balance, but only if there are sufficient funds in the account.
+
+    class BankAccount
+    {
+    }
+
+    //Task 3: Building a Student Class with Overloaded Constructors
+
+    //Create a Student class. The class should have the following private properties:
+    //  name
+    //  grade
+    //  and a PUBLIC property SchoolName.
+    // 1)Implement a constructor that takes no parameters and sets name to "Unnamed", grade to 1, and schoolName to "Unknown".
+
+    // 2)Implement another constructor that takes one parameter for name and sets grade to 1, and schoolName to "Unknown".
+    //  Use the this keyword to navigate from this constructor to the first constructor when no name is given.
+    // 3) Implement another constructor that takes two parameters, one for name and one for grade, and sets schoolName to "Unknown".
+    //  Use the this keyword to navigate from this constructor to the second constructor when only name is given, and to the first constructor when no parameters are given.
+    // 4) Implement a final constructor that takes all three parameters and sets the corresponding properties.
+    //  Use the this keyword to navigate from this constructor to the third constructor when schoolName is not given, to the second constructor when only name is given, and to the first constructor when no parameters are given.
+    // 5) Implement public getters and setters for each of these properties.
+    //  Make sure that the setter methods validate the inputs (for example, name should not be an empty string, grade should be between 1 and 12).
+
+    class Student
+    {
+
     }
 }
