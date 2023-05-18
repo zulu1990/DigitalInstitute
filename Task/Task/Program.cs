f7ffd7c21e671c8a5a5c6ff3b66ec95e29d2311d
Beka Pukhashvili <beka.pukhashvili@dataart.com>
2023-05-19 00:00:23 +0400

    Revert "better formatted"
    This reverts commit e59212d2d475d44583b34e3385ae679a56b27e40.

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -8,45 +8,45 @@
         }
     }
 
-    //  Task 1: Create a simple Employee class.
-    //  The class should have the following private properties:
-    //  firstName,
-    //  lastName,
-    //  employeeId,
-    //  salary,
-    //  role.
+    //Task 1
+    //Create a simple Employee class. The class should have the following private properties:
+    //firstName,
+    //lastName,
+    //employeeId,
+    //salary,
+    //role.
 
-    //  Implement a constructor that takes these values as parameters and initializes the properties.
-    //  Implement public getters and setters for each of these properties.
-    //  Make sure that the setter methods validate the inputs (for example, employeeId and salary cannot be negative, names should not be empty, etc.).
-    //  Implement a method getFullName() that returns the full name of the employee.
+    //Implement a constructor that takes these values as parameters and initializes the properties.
+    //Implement public getters and setters for each of these properties.
+    //Make sure that the setter methods validate the inputs (for example, employeeId and salary cannot be negative, names should not be empty, etc.).
+    //Implement a method getFullName() that returns the full name of the employee.
 
     class Employee
     {
 
     }
 
-    //  Task 2: Create a BankAccount class.
-    //  The class should have the following private properties:
-    //  accountNumber,
-    //  ownerName,
-    //  balance.
+    //Task 2
+    //Create a BankAccount class. The class should have the following private properties:
+    //accountNumber,
+    //ownerName,
+    //balance.
 
-    //  Implement a constructor that takes these values as parameters and initializes the properties.
-    //  The initial balance should be 0.
+    //Implement a constructor that takes these values as parameters and initializes the properties.
+    //The initial balance should be 0.
 
-    //  Implement public getters and setters for accountNumber and ownerName, but only a getter for balance(the balance should not be directly settable).
-    //  Implement two methods: deposit(amount) and withdraw(amount).
-    //  The deposit(amount) method should add the amount to the balance.
-    //  The withdraw(amount) method should subtract the amount from the balance, but only if there are sufficient funds in the account.
+    //Implement public getters and setters for accountNumber and ownerName, but only a getter for balance(the balance should not be directly settable).
+    //Implement two methods: deposit(amount) and withdraw(amount).
+    //The deposit(amount) method should add the amount to the balance.
+    //The withdraw(amount) method should subtract the amount from the balance, but only if there are sufficient funds in the account.
 
     class BankAccount
     {
     }
 
-    //  Task 3: Building a Student Class with Overloaded Constructors
+    //Task 3: Building a Student Class with Overloaded Constructors
 
-    //  Create a Student class. The class should have the following private properties:
+    //Create a Student class. The class should have the following private properties:
     //  name
     //  grade
     //  and a PUBLIC property SchoolName.
