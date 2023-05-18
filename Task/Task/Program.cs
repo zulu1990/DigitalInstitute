3403414b8dbb9f9ef253c62cdd84785fb0ba7572
n1nni <nin.dautashvili@gmail.com>
2023-05-18 22:04:44 +0400

    solve everything

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Task
+﻿using System.Data;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Test class Employee
+            //Employee employee1 = new Employee("John", "Doe", 1, 3590m, "Developer");
+            //employee1.GetFullName();
+            //employee1.FirstName = "";
+            //Console.WriteLine(employee1.FirstName);
+
+            // Test class BankAccount
+            BankAccount bankAccount = new BankAccount(3, "John", 20m);
+            decimal deposit = bankAccount.Deposit(30m);
+            Console.WriteLine(deposit);
+            decimal withraw = bankAccount.Withdraw(80m);
+            Console.WriteLine(withraw);
+
+            // Test class Student
+            Student student = new Student();
+            string name = student.Name;
+            double grade = student.Grade;
+            string schoolame = student.SchoolName;
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+            Console.WriteLine(schoolame);
+            student.Grade = 15;
+            Console.WriteLine(grade);
+
+            Student student2 = new Student("John");
+
         }
     }
 
@@ -23,7 +52,90 @@
 
     class Employee
     {
+        // fields
+        private string _firstName;
+        private string _lastName;
+        private int _employeeId;
+        private decimal _salary;
+        private string _role;
 
+        public Employee(string firstname, string lastname, int employeeId, decimal salary, string role)
+        {
+            _firstName = firstname;
+            _lastName = lastname;
+            _employeeId = employeeId;
+            _salary = salary;
+            _role = role;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Not a name");
+                }
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Not a lastname");
+                }
+                _lastName = value;
+            }
+        }
+
+        public int EmployeeId
+        {
+            get => _employeeId;
+            set
+            {
+                if (value <= 0)
+                {
+                    new ArgumentException("less or equal to 0");
+                }
+                _employeeId = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value >= 0)
+                {
+                    _salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("less or equal to 0");
+                }
+
+            }
+        }
+        public string Role
+        {
+            get => _role;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Null or empty value");
+                }
+                _role = value;
+            }
+        }
+
+        public void GetFullName() => Console.WriteLine($"{_firstName} {_lastName}");
     }
 
     //  Task 2: Create a BankAccount class.
@@ -42,6 +154,56 @@
 
     class BankAccount
     {
+        private int _accountNumber;
+        private string _ownerName;
+        private decimal _balance;
+
+        public BankAccount(int accountNumber, string ownerName, decimal balance = 0)
+        {
+            _accountNumber = accountNumber;
+            _ownerName = ownerName;
+            _balance = balance;
+        }
+
+        public int AccountNumber
+        {
+            get => _accountNumber;
+            set
+            {
+                if (value > 0) _accountNumber = value;
+                else throw new ArgumentException("less than 0");
+            }
+        }
+        public string OwnerName
+        {
+            get => _ownerName;
+            set
+            {
+                if (String.IsNullOrEmpty(value)) throw new ArgumentNullException("No name");
+                else _ownerName = value;
+            }
+        }
+        public decimal Balance
+        {
+            get => _balance;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            return _balance += amount;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                Console.WriteLine("Not enough money");
+            }
+            else
+            {
+                _balance -= amount;
+            }
+            return Balance;
+        }
     }
 
     //  Task 3: Building a Student Class with Overloaded Constructors
@@ -63,6 +225,48 @@
 
     class Student
     {
+        private string _name;
+        private double _grade;
+        public string SchoolName { get; set; }
 
+        public Student()
+        {
+            _name = "Unnamed";
+            _grade = 1;
+            SchoolName = "Unknown";
+        }
+
+        public Student(string name) : this()
+        {
+            _name = name;
+        }
+        public Student(string name, double grade) : this(name)
+        {
+            _grade = grade;
+        }
+        public Student(string name, double grade, string schoolName) : this(name, grade)
+        {
+            SchoolName = schoolName;
+        }
+
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (String.IsNullOrEmpty(value)) throw new ArgumentNullException("Not a name");
+                else _name = value;
+            }
+        }
+        public double Grade
+        {
+            get => _grade;
+            set
+            {
+                if (value < 1 || value > 12) throw new ArgumentException("grade should be between 1-12");
+                else _grade = value;
+            }
+        }
     }
 }
