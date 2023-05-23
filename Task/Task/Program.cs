49f9b807af3f360a48ebf8b65fcedcb9c2e38afa
n1nni <nin.dautashvili@gmail.com>
2023-05-23 21:55:38 +0400

    solve everything

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -9,11 +9,11 @@ namespace Task
         static void Main(string[] args)
         {
             Bird bird = new Bird();
-            Console.WriteLine(bird.ToString());
+            Console.WriteLine(bird.GetDetails());
             Bird bird1 = new Bird("coco");
             Console.WriteLine(bird1.ToString());
             Bird bird2 = new Bird("loco", 2, "white");
-            Console.WriteLine(bird2.ToString());
+            Console.WriteLine(bird2.GetDetails());
             Cat cat = new Cat();
             cat.Move();
             Fish fish = new Fish();
@@ -29,9 +29,34 @@ namespace Task
 
     abstract class Animal
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Color { get; set; }
+        public string Name { 
+            get => Name;
+            set {
+                if (String.IsNullOrWhiteSpace(value)){
+                    throw new ArgumentNullException("Empty value");
+                }
+            } 
+        }
+        public int Age {
+            get => Age;
+            set
+            {
+                if (Age<0)
+                {
+                    throw new ArgumentOutOfRangeException("Age can't be less than 0");
+                }
+            }
+        }
+        public string Color {
+            get => Color;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Empty value");
+                }
+            }
+        }
 
         public virtual void Move() => Console.WriteLine("The animal moves");
     }
@@ -56,32 +81,68 @@ namespace Task
             Console.WriteLine("The bird flies");
         }
 
+        public string GetDetails()
+        {
+            return $"{nameof(Name)}: {base.Name}, {nameof(Age)}: {base.Age}, {nameof(Color)}: {base.Color}";
+        }
+        //override ToString
         public override string ToString()
         {
-            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}, {nameof(Color)}: {Color}";
+            return $"{nameof(Name)}: {Name},\n{nameof(Age)}: {Age},\n{nameof(Color)}: {Color}";
         }
     }
-    class Cat : Animal
+    sealed class Cat : Animal
     {
         public override void Move()
         {
             base.Move();
-            Console.WriteLine("The Cat Meaws");
+            Console.WriteLine("The Cat runs");
         }
     }
+    //class Lion : Cat() // class of Cat can't be inherited
+    //{
+
+    //}
+
     class Fish : Animal
     {
+        public Fish() : base()
+        {
+            Name = "Nemo";
+            Age = 2;
+            Color = "Colorful";
+        }
+
+        public Fish(string name) : this()
+        {
+            Name = name;
+        }
+
+        public Fish(string name, int age) : this(name)
+        {
+            Age = age;
+        }
+        public Fish(string name, int age, string color):this(name, age)
+        {
+            Color = color;
+        }
+
+
         public override void Move()
         {
             base.Move();
             Console.WriteLine("The fish swims");
         }
+
+
+        
     }
 
     // 2) Using 'sealed' Keyword
     //Extend the above task.Make the 'Cat' class sealed.
     //Then try to inherit from 'Cat' and create another class 'Lion'.
     //The code should produce a compile-time error because a sealed class can't be inherited.
+
 
     // 3) Using 'base' Keyword
 
