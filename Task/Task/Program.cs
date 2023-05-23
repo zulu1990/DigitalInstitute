e326cb642ddd3ec45ec5969a374cb66ccbae367e
Beka Pukhashvili <beka.pukhashvili@dataart.com>
2023-05-23 11:34:16 +0400

    added task18

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Task
+﻿using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
+    // 1) Creating a Base Class and Derived Classes
+
+    //Create an abstract base class called 'Animal' with properties like 'Name', 'Age' and 'Color'.
+    //Implement a virtual method 'Move' which just outputs a default moving behavior(e.g., "The animal moves").
+    //Now, create three derived classes 'Bird', 'Cat', and 'Fish'.
+    //Override the 'Move' method in each derived class to output a specific moving behavior(e.g., "The bird flies", "The cat runs", "The fish swims").
+
+    // 2) Using 'sealed' Keyword
+    //Extend the above task.Make the 'Cat' class sealed.
+    //Then try to inherit from 'Cat' and create another class 'Lion'.
+    //The code should produce a compile-time error because a sealed class can't be inherited.
+
+    // 3) Using 'base' Keyword
+
+    //Extend the first task even further.
+    //In the 'Bird', 'Cat', and 'Fish' classes, besides overriding the 'Move' method,
+    //also create another method called 'GetDetails'.
+    //This method should output the details of the animal(name, age, and color).
+    //In this 'GetDetails' method, use the base keyword to access properties of the base class.
+
+    // 4) Try modeling other stuff with your knowledge (as much as you can!
 }
