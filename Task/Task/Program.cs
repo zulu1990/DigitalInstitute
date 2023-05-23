26c235c991234d5f0351806a637a35c02a494285
Jaghmaidze <nikoloz.jaghmaidze@gmail.com>
2023-05-23 15:06:01 +0400

    everything solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Task
+﻿using System.Data;
+
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
         }
     }
 
@@ -23,6 +26,86 @@
 
     class Employee
     {
+        private string _firstname;
+        private string _lastname;
+        private int _employeeId;
+        private decimal _salary;
+        private string _role;
+
+        public Employee(string firstName, string lastName, int employeeId, decimal salary, string role)
+        {
+            _firstname = firstName;
+            _lastname = lastName;
+            _employeeId = employeeId;
+            _salary = salary;
+            _role = role;
+        }
+
+        public string firstName
+        {
+            get { return _firstname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("You Didnt Entered The Name Try Again: ");
+
+                _firstname = value;
+            }
+        }
+
+        public string lastName
+        {
+            get { return _lastname; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("You Didnt Entered The LastName");
+
+                _lastname = value; 
+            }
+        }
+
+        public int employeeId
+        {
+            get { return _employeeId; }
+            set 
+            {
+                if (_employeeId < 0)
+                    throw new Exception("EmployeeId Is Less Than 0");
+
+                _employeeId = value; 
+            }
+        }
+
+        public decimal salary
+        {
+            get { return _salary; }
+            set 
+            {
+                if (_salary < 0)
+                    throw new Exception("Salary Is Less Than 0");
+
+                _salary = value; 
+            }
+        }
+
+        public string role
+        {
+            get { return _role; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("You Didnt Entered The Role");
+
+                _role = value; 
+            }
+        }
+
+        public string GetFullName()
+        {
+            return $"{firstName} {lastName}";
+        }
+
 
     }
 
@@ -42,6 +125,51 @@
 
     class BankAccount
     {
+        private decimal _accountNumber;
+        private string _ownerName;
+        private decimal _balance;
+
+        public BankAccount(decimal accountNumber, string ownerName)
+        {
+            _accountNumber = accountNumber;
+            _ownerName = ownerName;
+            _balance = 0;
+        }
+
+        public decimal accaountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+
+        public string ownerName
+        {
+            get { return _ownerName; }
+            set { _ownerName = value; }
+        }
+
+        public decimal balance
+        {
+            get { return _balance; }
+        }
+
+        public void Deposit(decimal amount)
+        {
+           _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                _balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds in the account.");
+            }
+
+        }
     }
 
     //Task 3: Building a Student Class with Overloaded Constructors
@@ -61,8 +189,63 @@
     // 5) Implement public getters and setters for each of these properties.
     //  Make sure that the setter methods validate the inputs (for example, name should not be an empty string, grade should be between 1 and 12).
 
-    class Student
+    public class Student
     {
+        private string name;
+        private int grade;
+        public string SchoolName { get; set; }
 
+        public Student()
+            : this("Unnamed", 1, "Unknown")
+        {
+        }
+
+        public Student(string name)
+            : this(name, 1, "Unknown")
+        {
+        }
+
+        public Student(string name, int grade)
+            : this(name, grade, "Unknown")
+        {
+        }
+
+        public Student(string name, int grade, string schoolName)
+        {
+            SetName(name);
+            SetGrade(grade);
+            SchoolName = schoolName;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Name cannot be empty.");
+            }
+
+            name = value;
+        }
+
+        public int GetGrade()
+        {
+            return grade;
+        }
+
+        public void SetGrade(int value)
+        {
+            if (value < 1 || value > 12)
+            {
+                throw new ArgumentException("Grade should be between 1 and 12.");
+            }
+
+            grade = value;
+        }
     }
+
 }
