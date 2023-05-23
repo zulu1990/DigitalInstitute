e6bc585f827d44aa376ae68e2f0d9b4e79e62713
Mikheili Chkhvirkia <91428850+MikheiliChkhvirkia@users.noreply.github.com>
2023-05-23 22:17:31 +0400

    Everything solved

---
+++ Task/Task/Games.cs
@@ -0,0 +1,258 @@
+﻿namespace Task
+{
+    public abstract class Game
+    {
+        protected string _name;
+        protected int _playerCount;
+
+        public Game(string name, int playerCount)
+        {
+            _name = name;
+            _playerCount = playerCount;
+        }
+
+        public abstract void Play();
+    }
+
+    public class TicTacToe : Game
+    {
+        public TicTacToe() : base("Tic-Tac-Toe", 2)
+        {
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"Playing {_name} with {_playerCount} players");
+
+            #region Game Logic
+
+            char[,] board = new char[3, 3];
+            char currentPlayer = 'X';
+            bool gameWon = false;
+            InitializeBoard();
+
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+
+            while (!gameWon)
+            {
+                PrintBoard();
+                Console.WriteLine($"Player {currentPlayer}'s turn. Enter the row (0-2):");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter the column (0-2):");
+                int column = Convert.ToInt32(Console.ReadLine());
+
+                if (IsValidMove(row, column))
+                {
+                    MakeMove(row, column);
+                    CheckWinningCondition();
+                    SwitchPlayer();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move. Try again.");
+                }
+            }
+
+            PrintBoard();
+            Console.WriteLine($"Player {currentPlayer} wins the game!");
+            void InitializeBoard()
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int column = 0; column < 3; column++)
+                    {
+                        board[row, column] = '-';
+                    }
+                }
+            }
+
+            void PrintBoard()
+            {
+                Console.WriteLine("Board:");
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int column = 0; column < 3; column++)
+                    {
+                        Console.Write(board[row, column] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            bool IsValidMove(int row, int column)
+            {
+                if (row >= 0 && row < 3 && column >= 0 && column < 3 && board[row, column] == '-')
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            void MakeMove(int row, int column)
+            {
+                board[row, column] = currentPlayer;
+            }
+
+            void CheckWinningCondition()
+            {
+                // Check rows
+                for (int row = 0; row < 3; row++)
+                {
+                    if (board[row, 0] != '-' && board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                    {
+                        gameWon = true;
+                        return;
+                    }
+                }
+
+                // Check columns
+                for (int column = 0; column < 3; column++)
+                {
+                    if (board[0, column] != '-' && board[0, column] == board[1, column] && board[1, column] == board[2, column])
+                    {
+                        gameWon = true;
+                        return;
+                    }
+                }
+
+                // Check diagonals
+                if (board[0, 0] != '-' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                {
+                    gameWon = true;
+                    return;
+                }
+
+                if (board[0, 2] != '-' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                {
+                    gameWon = true;
+                    return;
+                }
+
+                // Check for a tie
+                bool tie = true;
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int column = 0; column < 3; column++)
+                    {
+                        if (board[row, column] == '-')
+                        {
+                            tie = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (tie)
+                {
+                    gameWon = true;
+                    Console.WriteLine("It's a tie!");
+                }
+            }
+
+            void SwitchPlayer()
+            {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+
+            #endregion
+
+            Console.WriteLine("Tic-Tac-Toe game in progress...");
+            Console.WriteLine("Game over! Winner: Player 1");
+        }
+    }
+
+    public class Chess : Game
+    {
+        public Chess() : base("Chess", 2)
+        {
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"Playing {_name} with {_playerCount} players");
+            // Chess game logic 
+            Console.WriteLine("Chess game in progress...");
+            Console.WriteLine("Game over! Winner: Player 2");
+        }
+    }
+
+    public class SnakeAndLadder : Game
+    {
+        public SnakeAndLadder(int playerCount) : base("Snake and Ladder", playerCount)
+        {
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"Playing {_name} with {_playerCount} players");
+            Console.WriteLine("Snake and Ladder game in progress...");
+            #region Game Logic
+
+            int[] playerPositions;
+            int[] diceValues;
+            bool[] isPlayerFinished;
+            int playerCount;
+            bool gameOver;
+
+            playerCount = _playerCount;
+            playerPositions = new int[playerCount];
+            diceValues = new int[playerCount];
+            isPlayerFinished = new bool[playerCount];
+            gameOver = false;
+
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+            Console.WriteLine("Number of players: " + playerCount);
+
+            while (!gameOver)
+            {
+                for (int i = 0; i < playerCount; i++)
+                {
+                    if (!isPlayerFinished[i])
+                    {
+                        RollDice(i);
+                        MovePlayer(i);
+                        CheckWinningCondition(i);
+
+                        if (gameOver)
+                            break;
+                    }
+                }
+            }
+
+            void RollDice(int playerIndex)
+            {
+                Random random = new Random();
+                diceValues[playerIndex] = random.Next(1, 7);
+                Console.WriteLine("Player " + (playerIndex + 1) + " rolled a " + diceValues[playerIndex]);
+            }
+
+            void MovePlayer(int playerIndex)
+            {
+                playerPositions[playerIndex] += diceValues[playerIndex];
+                Console.WriteLine("Player " + (playerIndex + 1) + " current position: " + playerPositions[playerIndex]);
+            }
+
+            void CheckWinningCondition(int playerIndex)
+            {
+                if (playerPositions[playerIndex] >= 100)
+                {
+                    gameOver = true;
+                    isPlayerFinished[playerIndex] = true;
+                }
+            }
+
+            int GetWinningPlayer()
+            {
+                for (int i = 0; i < playerCount; i++)
+                {
+                    if (playerPositions[i] >= 100)
+                        return i + 1;
+                }
+                return -1;
+            }
+
+            Console.WriteLine("Congratulations! Player " + GetWinningPlayer() + " wins the game!");
+            #endregion
+        }
+    }
+}
--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,16 +1,131 @@
-﻿using System.Runtime.Intrinsics.X86;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Task
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        #region Classes
+
+        public enum ColorType
+        {
+            White,
+            Black,
+            Red,
+            Green,
+            Blue,
+            Yellow,
+        }
+
+        public abstract class Animal
+        {
+            protected string _name { get; set; }
+            protected int _age { get; set; }
+            protected ColorType _colorTypeId { get; set; }
+
+            public Animal(string name, int age, ColorType colorTypeId)
+            {
+                _name = name;
+                _age = age;
+                _colorTypeId = colorTypeId;
+            }
+
+            public abstract void Move();
+        }
+
+        public class Bird : Animal
+        {
+            public Bird(string name, int age, ColorType colorTypeId) : base(name, age, colorTypeId)
+            {
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The bird flies");
+            }
+
+            public void GetDetails()
+            {
+                Console.WriteLine($"Name: {_name}, Age: {_age}, Color: {Enum.GetName(typeof(ColorType), _colorTypeId)}");
+            }
+        }
+
+        public class Cat : Animal
+        {
+            public Cat(string name, int age, ColorType colorTypeId) : base(name, age, colorTypeId)
+            {
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The cat runs");
+            }
+
+            public void GetDetails()
+            {
+                Console.WriteLine($"Name: {_name}, Age: {_age}, Color: {Enum.GetName(typeof(ColorType), _colorTypeId)}");
+            }
+        }
+
+        public class Fish : Animal
+        {
+            public Fish(string name, int age, ColorType colorTypeId) : base(name, age, colorTypeId)
+            {
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The fish swims");
+            }
+
+            public void GetDetails()
+            {
+                Console.WriteLine($"Name: {_name}, Age: {_age}, Color: {Enum.GetName(typeof(ColorType), _colorTypeId)}");
+            }
+        }
+        class Lion
         {
 
         }
+        #endregion
+        // HomeWork 18
+        static void Main(string[] args)
+        {
+            Bird bird = new("Sparrow", 1, ColorType.Red);
+            bird.Move();
+            bird.GetDetails();
+            Console.WriteLine();
+
+            Cat cat = new("Tom", 2, ColorType.Black);
+            cat.Move();
+            cat.GetDetails();
+            Console.WriteLine();
+
+            Fish fish = new("Nemo", 1, ColorType.Blue);
+            fish.Move();
+            fish.GetDetails();
+        }
+
+        // 4 Bonus 
+        //public static void Main()
+        //{
+        //    Game ticTacToe = new TicTacToe();
+        //    Game chess = new Chess();
+        //    Console.Write("Write how many players will play(1 - 5): ");
+        //    Game snakeAndLadder = new SnakeAndLadder(int.Parse(Console.ReadLine()));
+
+        //    PlayGame(ticTacToe);
+        //    PlayGame(chess);
+        //    PlayGame(snakeAndLadder);
+        //}
+
+        //public static void PlayGame(Game game)
+        //{
+        //    game.Play();
+        //    Console.WriteLine();
+        //}
     }
+
     // 1) Creating a Base Class and Derived Classes
 
     //Create an abstract base class called 'Animal' with properties like 'Name', 'Age' and 'Color'.
