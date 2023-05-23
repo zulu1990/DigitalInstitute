f409253d1921c6e1afb4a5425cb7d84255fda6eb
n1nni <nin.dautashvili@gmail.com>
2023-05-23 14:55:52 +0400

    solve task 1

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -8,7 +8,16 @@ namespace Task
     {
         static void Main(string[] args)
         {
-
+            Bird bird = new Bird();
+            Console.WriteLine(bird.ToString());
+            Bird bird1 = new Bird("coco");
+            Console.WriteLine(bird1.ToString());
+            Bird bird2 = new Bird("loco", 2, "white");
+            Console.WriteLine(bird2.ToString());
+            Cat cat = new Cat();
+            cat.Move();
+            Fish fish = new Fish();
+            fish.Move();
         }
     }
     // 1) Creating a Base Class and Derived Classes
@@ -17,6 +26,57 @@ namespace Task
     //Implement a virtual method 'Move' which just outputs a default moving behavior(e.g., "The animal moves").
     //Now, create three derived classes 'Bird', 'Cat', and 'Fish'.
     //Override the 'Move' method in each derived class to output a specific moving behavior(e.g., "The bird flies", "The cat runs", "The fish swims").
+
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public virtual void Move() => Console.WriteLine("The animal moves");
+    }
+
+    class Bird : Animal
+    {
+        public Bird():base()
+        {
+            Name = "Chico";
+            Age = 1;
+            Color = "Gray";
+        }
+        public Bird(string name, int age=0, string color="Black") : this()
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+        }
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine("The bird flies");
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}, {nameof(Color)}: {Color}";
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine("The Cat Meaws");
+        }
+    }
+    class Fish : Animal
+    {
+        public override void Move()
+        {
+            base.Move();
+            Console.WriteLine("The fish swims");
+        }
+    }
 
     // 2) Using 'sealed' Keyword
     //Extend the above task.Make the 'Cat' class sealed.
