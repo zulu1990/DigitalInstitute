bfd17e6501831d56a58995c5a0c73fd5fdf8b12c
DavitMamardashvili <mamardashvilidavit7@gmail.com>
2023-05-24 04:25:46 +0400

    Everything solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -8,28 +8,95 @@ namespace Task
     {
         static void Main(string[] args)
         {
+            Bird bird = new Bird("jeka", 12, "mwvane");
+            bird.Move();
+            bird.GetDetails();
+        }
+
+        
+
+        abstract class Animal
+        {
+            protected string _name;
+            protected int _age;
+            protected string _color;
+
+            public Animal(string name, int age, string color)
+            {
+                _name = name;
+                _age = age;
+                _color = color;
+
+            }
+            public void GetDetails()
+            {
+                Console.WriteLine($"{nameof(_name)} : {_name} {Environment.NewLine}" +
+                                  $"{nameof(_age)} : {_age} {Environment.NewLine}" +
+                                  $"{nameof(_color)} : {_color} ");
+            }
+
+            public virtual void Move() => Console.WriteLine("The animal moves");
 
         }
+
+
+
+        class Bird : Animal
+        {
+            public Bird(string name, int age, string color) : base(name, age, color)
+            {
+
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The bird flies");
+                base.Move();
+            }
+
+
+        }
+
+
+
+        sealed class Cat : Animal
+        {
+            public Cat(string name, int age, string color) : base(name, age, color)
+            {
+
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The cat runs");
+                base.Move();
+            }
+
+
+        }
+
+
+        class Fish : Animal
+        {
+            public Fish(string name, int age, string color) : base(name, age, color)
+            {
+
+            }
+
+            public override void Move()
+            {
+                Console.WriteLine("The fish swims");
+                base.Move();
+            }
+
+
+
+        }
+
+
+
+
+
+
     }
-    // 1) Creating a Base Class and Derived Classes
-
-    //Create an abstract base class called 'Animal' with properties like 'Name', 'Age' and 'Color'.
-    //Implement a virtual method 'Move' which just outputs a default moving behavior(e.g., "The animal moves").
-    //Now, create three derived classes 'Bird', 'Cat', and 'Fish'.
-    //Override the 'Move' method in each derived class to output a specific moving behavior(e.g., "The bird flies", "The cat runs", "The fish swims").
-
-    // 2) Using 'sealed' Keyword
-    //Extend the above task.Make the 'Cat' class sealed.
-    //Then try to inherit from 'Cat' and create another class 'Lion'.
-    //The code should produce a compile-time error because a sealed class can't be inherited.
-
-    // 3) Using 'base' Keyword
-
-    //Extend the first task even further.
-    //In the 'Bird', 'Cat', and 'Fish' classes, besides overriding the 'Move' method,
-    //also create another method called 'GetDetails'.
-    //This method should output the details of the animal(name, age, and color).
-    //In this 'GetDetails' method, use the base keyword to access properties of the base class.
-
-    // 4) Try modeling other stuff with your knowledge (as much as you can!
 }
