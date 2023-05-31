1d988ec92617a8bff7a0e799d663ae8679348eb5
DESKTOP-9FT8NSD\THERE WAS PIPIKO <lukakashia10@gmail.com>
2023-05-31 09:36:13 -0700

    everything solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -15,7 +15,16 @@
         //The sum of the digits of the number 1234 is : 10
         public static int SumOfNumbers()
         {
-            throw new NotImplementedException();
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            return sum;
         }
 
         //Write a method in C# Sharp to create a recursive function to calculate the Fibonacci number of a specific term.
@@ -24,15 +33,36 @@
         //Expected Output :
         //The Fibonacci of 10 th term is 55
         //The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones, often starting with 0 and 1. That is, the sequence goes: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
-        public static int Fibonacci()
+        public static int Fibonacci(int n)
         {
-            throw new NotImplementedException();
+            if(n <= 1)
+            
+               return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+            
         }
 
         // Write C# method to draw the Chess board in console
         public static void DrawChessBoard()
         {
+            int boardSize = 8; // Size of the chessboard
+            int cellSize = 4; // Size of each cell in characters
 
-        }
+            for (int row = 0; row < boardSize; row++)
+            {
+                for (int col = 0; col < boardSize; col++)
+                {
+                    
+                    int xPos = col * cellSize;
+                    int yPos = row * cellSize;
+
+                    
+                    Console.SetCursorPosition(xPos, yPos);
+                    Console.BackgroundColor = (row + col) % 2 == 0 ? ConsoleColor.White : ConsoleColor.Black;
+                    Console.ForegroundColor = (row + col) % 2 != 0 ? ConsoleColor.White : ConsoleColor.Black;
+                    Console.Write(new string(' ', cellSize));
+                }
+            }
     }
 }
