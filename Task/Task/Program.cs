a3c33fbfbca2c04863114a40f76c9d80792585c7
Beka Pukhashvili <da_bpukhashvili@phreesia.com>
2023-06-08 16:27:35 +0400

    fix

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-
         }
+        //Task 1: Simple IEnumerable and IEnumerator Implementation
+        //Create a simple class MyIntegerRange that represents a range of integers and implements IEnumerable<int>.
+        //It should have two private fields for the start and end of the range.
+        //It should use IEnumerator to iterate from the start to the end of the range.
+
+        //Task 2: Using the 'yield' Keyword
+        //Modify the MyIntegerRange class from the first task to use the yield keyword in its GetEnumerator() method instead of manually implementing an IEnumerator.
+
+        //Task 3: Fibonacci Sequence with 'yield' (It's not that easy :P)
+        //Create a method IEnumerable<int> Fibonacci(int n) that returns an IEnumerable sequence of the first n numbers in the Fibonacci series using the 'yield' keyword.
     }
 }
