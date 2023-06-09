f06ba48750c24801ad8dad91c1af2c5172e8ab72
DavitMamardashvili <mamardashvilidavit7@gmail.com>
2023-06-09 23:38:07 +0400

    have done fib

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            foreach (var numbers in Fibonachi(10))
+            {
+                Console.WriteLine(numbers);
+
+            }
+
         }
         //Task 1: Simple IEnumerable and IEnumerator Implementation
         //Create a simple class MyIntegerRange that represents a range of integers and implements IEnumerable<int>.
@@ -13,7 +19,30 @@
         //Task 2: Using the 'yield' Keyword
         //Modify the MyIntegerRange class from the first task to use the yield keyword in its GetEnumerator() method instead of manually implementing an IEnumerator.
 
-        //Task 3: Fibonacci Sequence with 'yield' (It's not that easy :P)
-        //Create a method IEnumerable<int> Fibonacci(int n) that returns an IEnumerable sequence of the first n numbers in the Fibonacci series using the 'yield' keyword.
+       
+
+        public static IEnumerable<int> Fibonachi(int n)
+        {
+            //Task 3: Fibonacci Sequence with 'yield' (It's not that easy :P)
+            //Create a method IEnumerable<int> Fibonacci(int n) that returns an IEnumerable sequence of the first n numbers in the Fibonacci series using the 'yield' keyword.
+
+            if (n < 1)
+                throw new ArgumentException("takeing number is less then 1");
+
+
+            int x = 0;
+            int y = 1;
+
+            for(int i = 0; i < n; i++)
+            {
+                yield return y;
+
+
+                int z = x + y;
+                x = y;
+                y = z;
+            }
+        }
     }
+
 }
