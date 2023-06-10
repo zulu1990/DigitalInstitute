9666cb07457800137772876c69899e45a590002c
Beka Pukhashvili <da_bpukhashvili@phreesia.com>
2023-06-10 21:14:46 +0400

    task created

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -7,4 +7,14 @@
 
         }
     }
+
+    // Assume you have a website where there are two advertising slots.
+    // With every refresh of the website, different advertisements will appear in these two slots.
+    // For instance, upon the first refresh, there might be ads for Facebook and Google.
+    // On the second refresh, there may be ads for Apple and Microsoft, and so forth.
+    // The frequency at which an ad appears is proportional to the amount paid by the advertiser;
+    // for example, if Microsoft pays $10,000 and Apple pays $15, Microsoft's ads will appear more often.
+    // Now onto the task:
+    // You are required to write a method that will randomly return two ads from a dictionary.
+    // The likelihood of an ad being returned should be based on the price the advertiser has paid for it.
 }
