4ddd9433775a8e50edb47980c97842fa0ac0ca5b
Giorgi Koghuashvili <giorgikoguashvili@gmail.com>
2023-06-14 03:57:21 +0400

    Create first steps.

---
+++ Task/Task/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class MyClass<T,U>
+    {
+        public T First { get; set; }
+        public U Second { get; set; }
+
+        public void SetValues(T firs, U second)
+        {
+            First = firs;
+            Second = second;
+        }
+        public (T firs, U second) GetValues()
+        {
+            return( First, Second);
+        }
+
+    }
+}
