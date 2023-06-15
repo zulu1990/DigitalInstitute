a971638fbe6ad70c708bdacd5d49c06713693832
Mikheili Chkhvirkia <91428850+MikheiliChkhvirkia@users.noreply.github.com>
2023-06-16 02:16:41 +0400

    solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -2,19 +2,94 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly Dictionary<string, int> dictionary = new()
         {
+            { "Facebook", 5000 },      // 11
+            { "Google", 8000 },        // 8
+            { "Apple", 10000 },        // 4
+            { "Microsoft", 15000 },    // 1
+            { "Amazon", 12000 },       // 2
+            { "Netflix", 9000 },       // 6
+            { "Tesla", 11000 },        // 3
+            { "IBM", 7000 },           // 9
+            { "Samsung", 9500 },       // 7
+            { "Intel", 6000 }          // 10
+        };
 
+        private static readonly int count = dictionary.Count;
+        private const int bannerCount = 2;
+
+        static void Main()
+        {
+            Console.WriteLine("Random Ads:\n");
+
+            foreach (string ad in GetRandomAds())
+            {
+                Console.WriteLine($"{ad}\n");
+            }
+        }
+
+        private static List<string> GetRandomAds()
+        {
+            List<string> randomAds = new ();
+            List<int> randomIndexes = GenerateRandomIndexes();
+
+            foreach (int index in randomIndexes)
+            {
+                string ad = GetAdAtIndex(index);
+                randomAds.Add(ad);
+            }
+
+            return randomAds;
+        }
+
+        private static List<int> GenerateRandomIndexes()
+        {
+            List<int> weights = dictionary.Values.ToList();
+            List<int> randomIndexes = new(bannerCount);
+            Random random = new();
+
+            while (randomIndexes.Count < bannerCount)
+            {
+                int totalWeight = weights.Sum();
+                int randomNumber = random.Next(totalWeight);
+
+                int cumulativeWeight = 0;
+                int currentIndex = 0;
+
+                while (currentIndex < weights.Count)
+                {
+                    cumulativeWeight += weights[currentIndex];
+
+                    if (randomNumber < cumulativeWeight)
+                    {
+                        randomIndexes.Add(currentIndex);
+                        weights.RemoveAt(currentIndex);
+                        break;
+                    }
+
+                    currentIndex++;
+                }
+            }
+
+            return randomIndexes;
+        }
+
+        private static string GetAdAtIndex(int index)
+        {
+            int currentIndex = 0;
+
+            foreach (string ad in dictionary.Keys)
+            {
+                if (currentIndex == index)
+                {
+                    return ad;
+                }
+
+                currentIndex++;
+            }
+
+            throw new IndexOutOfRangeException("Invalid index");
         }
     }
-
-    // Assume you have a website where there are two advertising slots.
-    // With every refresh of the website, different advertisements will appear in these two slots.
-    // For instance, upon the first refresh, there might be ads for Facebook and Google.
-    // On the second refresh, there may be ads for Apple and Microsoft, and so forth.
-    // The frequency at which an ad appears is proportional to the amount paid by the advertiser;
-    // for example, if Microsoft pays $10,000 and Apple pays $15, Microsoft's ads will appear more often.
-    // Now onto the task:
-    // You are required to write a method that will randomly return two ads from a dictionary.
-    // The likelihood of an ad being returned should be based on the price the advertiser has paid for it.
 }
