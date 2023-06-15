2e6eea65177f9e46e1ed17ac81e83eeb3b1ff4b2
Mikheili Chkhvirkia <91428850+MikheiliChkhvirkia@users.noreply.github.com>
2023-06-15 16:02:41 +0400

    solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,47 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-
-namespace Task
+﻿namespace Task
 {
+    // Task 1
+    // Generic Classes
+    public class Pair<T, U>
+    {
+        private T First { get; set; }
+        private U Second { get; set; }
+
+        public Pair(T first, U second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public (T, U) GetValues()
+        {
+            return (First, Second);
+        }
+    }
+
+    // Task 2
+    // Generic Methods
+    public static class SwapUtility
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            //T temp = a;
+            //a = b;
+            //b = temp;
+            (a, b) = (b, a);
+        }
+    }
+
+    // Task 3
+    // Constraints in Generics
+    public class SortedCollection<T> where T : IComparable<T>
+    {
+        private readonly List<T> items;
+
+        public SortedCollection()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+            items.Sort();
+        }
+
+        public T GetItem(int index)
+        {
+            if (index >= 0 && index < items.Count)
+                return items[index];
+
+            throw new IndexOutOfRangeException("Invalid index");
+        }
+
+        public int Count()
+           => items.Count;
+        
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Task 1
+            // Pair<T, U> class demonstration
+            Pair<int, string> pair1 = new Pair<int, string>(1, "One");
+            Console.WriteLine(pair1.GetValues());
 
+            Pair<float, string> pair2 = new Pair<float, string>(3.14f, "Pi");
+            Console.WriteLine(pair2.GetValues());
+            Console.WriteLine();
+
+            // Task 2
+            // SwapUtility class demonstration
+            int num1 = 10, num2 = 20;
+            Console.WriteLine($"Before swap: num1 = {num1}, num2 = {num2}");
+            SwapUtility.Swap(ref num1, ref num2);
+            Console.WriteLine($"After swap: num1 = {num1}, num2 = {num2}");
+
+            string str1 = "Hello", str2 = "World";
+            Console.WriteLine($"Before swap: str1 = {str1}, str2 = {str2}");
+            SwapUtility.Swap(ref str1, ref str2);
+            Console.WriteLine($"After swap: str1 = {str1}, str2 = {str2}");
+            Console.WriteLine();
+            // Task 3
+            // SortedCollection<T> class demonstration
+            SortedCollection<int> sortedInts = new();
+            sortedInts.AddItem(5);
+            sortedInts.AddItem(2);
+            sortedInts.AddItem(10);
+            sortedInts.AddItem(8);
+            sortedInts.AddItem(9);
+            for (int i = 0; i < sortedInts.Count(); i++)
+            {
+                Console.WriteLine(sortedInts.GetItem(i));
+            }
         }
     }
-    //Task 1
-    //Generic Classes:
-    //Create a generic class named Pair<T, U>.This class should represent a pair of two different types.
-    //It should have two properties - First and Second - to hold the pair of items.
-
-    //Include methods in the class:
-
-    //SetValues(T first, U second): This method should set the values of First and Second.
-    //GetValues(): This method should return a tuple of First and Second.
-    //Test the Pair<T, U> class using different pair of data types(like integer and string, float and string etc.)
-    //and demonstrate its functionality.
-
-
-    //Task 2
-    //Generic Methods:
-    //Define a generic method Swap<T> within a non-generic class SwapUtility.
-    //This method should take two references(ref parameters) to variables of a generic type and swap their values.
-    //Create a test program which demonstrates swapping values of different types (like integer, string, etc.).
-
-    //Task 3
-    //Constraints in Generics:
-    //Create a generic class SortedCollection<T> that maintains a sorted collection of items.
-    //Use the constraint on T such that T implements IComparable<T>.
-    //This is needed so that the items can be sorted.
-
-    //The class should have the following methods:
-    //AddItem(T item) : This method should add a new item to the collection and then ensure the collection remains sorted.
-    //GetItem(int index): This method should return the item at the given index.
-    //Test the SortedCollection<T> class with a simple custom class that implements IComparable<T>,
-    //and show how the items are always sorted when added to the collection.
-
-    //Task 4 do as much practice as you can with generics
-    //I think you liked the previous assignment. try to appy generics to the previous RPG game ;)
 }
