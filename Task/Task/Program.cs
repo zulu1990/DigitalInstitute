ea5960ed5d77393a43ef181ec22def23eac7b4f1
DESKTOP-9FT8NSD\THERE WAS PIPIKO <lukakashia10@gmail.com>
2023-06-20 17:13:30 -0700

    everything solved

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -23,7 +23,80 @@
 
     class Employee
     {
+        private string firstName;
+        private string lastName;
+        private int employeeId;
+        private decimal salary;
+        private string role;
 
+        public Employee(string firstName, string lastName, int employeeId, decimal salary, string role)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.employeeId = employeeId;
+            this.salary = salary;
+            this.role = role;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(firstName))
+                    throw new Exception(message: "Please Write something! ");
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(lastName))
+                    throw new Exception(message: "Please Write something! ");
+
+                lastName = value;
+            }
+        }
+        public int EmployeeId
+        {
+            get { return employeeId; }
+            set
+            {
+                if (employeeId < 0)
+                    throw new Exception(message: "ID must not be a negative number! ");
+
+                employeeId = value;
+
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (salary < 0)
+                    throw new Exception(message: "salary number must not be a negative number! ");
+
+                salary = value;
+            }
+        }
+        public string Role
+        {
+            get { return role; }
+            set
+            {
+                if (string.IsNullOrEmpty(role))
+                    throw new Exception(message: "Please Write something! ");
+
+                role = value;
+            }
+        }
+        public string getFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 
     //Task 2
@@ -42,6 +115,46 @@
 
     class BankAccount
     {
+        private int accountNumber;
+        private string owenerName;
+        private decimal balance;
+
+        public BankAccount(int accountNumber, string owenerName, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.owenerName = owenerName;
+            balance = 0;
+        }
+        public int AccountNumber
+        {
+            get { return accountNumber; }
+            set { accountNumber = value; }
+        }
+        public string OwnerName
+        {
+            get { return owenerName; }
+            set { owenerName = value; }
+        }
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public void deposit(int amount)
+        {
+            balance += amount;
+        }
+        public void withdraw(int amount)
+        {
+            if (amount > 0)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+        }
     }
 
     //Task 3: Building a Student Class with Overloaded Constructors
@@ -63,6 +176,70 @@
 
     class Student
     {
+        private string name;
+        private int grade;
+        public string SchoolName { get; set; }
 
+
+        public Student()
+
+        {
+            name = "Unnamed";
+            grade = 1;
+            SchoolName = "Unknown";
+        }
+        public Student(string name)
+            : this()
+
+        {
+            grade = 1; ;
+            SchoolName = "Unknown";
+        }
+
+        public Student(string name, int grade)
+            : this(name)          // aq ver gavige usaxelos rogor davukavshiro
+        {
+            SchoolName = "Unknown";
+        }
+        public Student(string name, int grade, string schoolName)
+            : this(name, grade)
+        {
+            name = "Unnamed";
+            grade = 1;
+            SchoolName = "Unknown";
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception(message: "Write something! ");
+                name = value;
+            }
+        }
+        public int Grade
+        {
+            get { return grade; }
+            set
+            {
+                if (grade < 1)
+                    throw new Exception(message: "Invalid grade number! ");
+                if (grade > 12)
+                    throw new Exception(message: "Invalid grade number! ");
+            }
+        }
+        public string SchoolNAme
+        {
+            get { return SchoolName; }
+            set
+            {
+                if (string.IsNullOrEmpty(SchoolName))
+                    throw new Exception(message: "Write Something! ");
+
+                SchoolName = value;
+            }
+        }
     }
 }
