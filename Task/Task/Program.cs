73ea36fd96ac0cb9cad0a7c6c6573e3f3653e6c6
Archili_Zivzivadze <archil.zivzivadze@gmail.com>
2023-07-04 11:39:01 +0400

    Update Program.cs

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            // algorithms
+            
+            // Task 1
+            // create random class with seed for example 10 (seeds are important to generate same values) var rand = new Random(10);
+            // generate list with 10000 random integer
+            // use different algorithm to find Max Value
+            //     1) use Sort method and then First to get max value
+            //     2) use prebuilt Max Method
+            //     3) use your own algorithm -> create function that takes list as parameter and returns maximum value. Do not use any prebuilt functions.
+            //     4) use Stopwatch class to get required time for each algorithm
 
+
+            // Task 2
+            // you have list that contains 50_000 integers from 1 to 1000
+            // write function that finds number that is repeated most times in list
+            // use stopwatch to calculate time took to complete task
         }
     }
 }
