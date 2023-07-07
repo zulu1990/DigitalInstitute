6728e25872c4c0fc52a0070038fb4d42525eab9b
davit iashvili <diashvili3@gmail.com>
2023-07-07 16:04:34 +0400

    Update Program.cs

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-           create a program that checks whether a given range of numbers is prime or not using multithreading.
-           The program should take an input range of numbers, split it into smaller chunks, and process each chunk concurrently using multithreading.
-           To achieve this, you'll use both threads and tasks.
+          // Task 28
+          // create a program that checks whether a given range of numbers is prime or not using multithreading.
+          // The program should take an input range of numbers, split it into smaller chunks, and process each chunk concurrently using multithreading.
+          // To achieve this, you'll use both threads and tasks.
         }
     }
 }
