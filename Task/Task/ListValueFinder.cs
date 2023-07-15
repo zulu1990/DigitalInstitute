15e624cd1dde24c23302d3e0c55c73ddfb4c616b
Iucha Megreladze <imegreladze.im@gmail.com>
2023-07-15 16:26:51 +0400

    everything solved

---
+++ Task/Task/ListValueFinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public static class ListValueFinder
+    {
+        
+        
+        public static int FindMax(this List<int> list)
+        {
+            int max = int.MinValue;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > max) max = list[i];
+            }
+
+            return max;
+        }
+
+
+        public static int FindMostFrequent(this List<int> list)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (map.ContainsKey(list[i]))
+                {
+                    map[list[i]]++;
+                }
+                else
+                {
+                    map.Add(list[i], 1);
+                }
+            }
+
+            int max = int.MinValue;
+            int num = 0;
+            foreach(KeyValuePair<int, int> pair in map)
+            {
+                if(pair.Value > max) max = pair.Value;
+                num = pair.Key;
+            }
+            return num;
+        }
+
+    }
+}
--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,25 +1,87 @@
-﻿namespace Task
+﻿using System.Diagnostics;
+
+namespace Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // algorithms
-            
-            // Task 1
-            // create random class with seed for example 10 (seeds are important to generate same values) var rand = new Random(10);
-            // generate list with 10000 random integer
-            // use different algorithm to find Max Value
-            //     1) use Sort method and then First to get max value
-            //     2) use prebuilt Max Method
-            //     3) use your own algorithm -> create function that takes list as parameter and returns maximum value. Do not use any prebuilt functions.
-            //     4) use Stopwatch class to get required time for each algorithm
+            //Task1
+            Task1();
 
 
-            // Task 2
-            // you have list that contains 50_000 integers from 1 to 1000
-            // write function that finds number that is repeated most times in list
-            // use stopwatch to calculate time took to complete task
+            //Task2
+            Task2();
         }
+
+        private static void Task1()
+        {
+            Random rand = new Random(10);
+
+            List<int> randomInts = new List<int>();
+            for(int i=0; i< 10000; i++) { randomInts.Add(rand.Next());}
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            //1
+            stopwatch.Start();
+            randomInts.Sort();
+            int max1 = randomInts.Last();
+            stopwatch.Stop();
+            Console.WriteLine("Seconds: "+stopwatch.Elapsed.TotalSeconds);
+            Console.WriteLine(max1);
+
+            //2
+            stopwatch.Start();
+            int max2 = randomInts.Max();
+            stopwatch.Stop();
+            Console.WriteLine("Seconds: " + stopwatch.Elapsed.TotalSeconds);
+            Console.WriteLine(max2);
+
+            //3
+            stopwatch.Start();
+            int max3 = randomInts.FindMax();
+            stopwatch.Stop();
+            Console.WriteLine("Seconds: " + stopwatch.Elapsed.TotalSeconds);
+            Console.WriteLine(max3);
+        }
+
+
+        private static void Task2()
+        {
+            Random random = new Random(10);
+            
+            List<int> list = new List<int>();
+            for(int i=0; i < 50_000; i++) { list.Add(random.Next(1,1000));}
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            int mostFrequent = list.FindMostFrequent();
+            stopwatch.Stop();
+            Console.WriteLine("Seconds: " + stopwatch.Elapsed.TotalSeconds);
+            Console.WriteLine(mostFrequent);
+
+        }
+
+
+        
+
+        // algorithms
+
+        // Task 1
+        // create random class with seed for example 10 (seeds are important to generate same values) var rand = new Random(10);
+        // generate list with 10000 random integer
+        // use different algorithm to find Max Value
+        //     1) use Sort method and then First to get max value
+        //     2) use prebuilt Max Method
+        //     3) use your own algorithm -> create function that takes list as parameter and returns maximum value. Do not use any prebuilt functions.
+        //     4) use Stopwatch class to get required time for each algorithm
+
+
+        // Task 2
+        // you have list that contains 50_000 integers from 1 to 1000
+        // write function that finds number that is repeated most times in list
+        // use stopwatch to calculate time took to complete task
     }
 }
